7c3f114a4e5fbf52248026774ec98b5d989bd57f
jhon <jhon.bocanegra26@gmail.com>
2023-02-24 11:20:50 -0500

    agregado boton agregar categoria

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,21 @@ export const App = () => {
 
   const [categories, setCategories] =  useState(['dragon ball', 'one punch'])
 
+  const onAddCategory = (categoryName) => {
+    const item = categories.find(item => item === categoryName)
+    if(item) {
+      return
+    }
+    setCategories([categoryName, ...categories])
+  }
+
   return (
     <div>
       <h1> Gifexpertapp </h1>
+      
+      <input type="text" />
+
+      <button onClick={() => onAddCategory('inuyasha')}> Agregar </button>
 
       <ul>
         {
